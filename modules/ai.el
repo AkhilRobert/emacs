84ec789a0876e339ef6d00fd78b04f0911f85882
Akhil Robert <akhilrobert2412@gmail.com>
2026-06-27 21:56:06 +0530

    Initial AI support

---
+++ modules/ai.el
@@ -0,0 +1,11 @@
+;;; ai.el --- AI Related configurations -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; AI Related configuration
+
+;;; Code:
+
+(use-package agent-shell
+  :ensure t)
+
+;;; ai.el ends here
